52616603fae541fef75042b834bf28b58f7562da
ShookieShookie <rhails@stanford.edu>
2017-01-19 15:03:58 -0800

    moved backend to 'server' folder

--- server/nodeServer/app.js
+++ server/nodeServer/app.js
@@ -2,27 +2,29 @@ var SMTPServer = require('smtp-server').SMTPServer;
 var options;
 var server = new SMTPServer({
     onConnect: function(session, callback){
-    	console.log("x")
+        console.log("x")
         if(session.remoteAddress === '127.0.0.1'){
             return callback(new Error('No connections from localhost allowed'));
         }
         return callback(); // Accept the connection
     },
-    onClose: function(){
-    	console.log("a connection closed")
+    onClose: function(session){
+        console.log("remote address" + session.remoteAddress)
+        console.log("transmission type" + session.transmissionType)
+        console.log("a connection closed")
     },
     onData: function() {
-    	console.log("data")
+        console.log("data")
     }
 
 });
 
 server.on('error',function(err) {
-	console.log(err)
+    console.log(err)
 })
 
 server.listen(8080,function(){
-	console.log("listening on port 8080!")
+    console.log("listening on port 8080!")
 })
 
 
